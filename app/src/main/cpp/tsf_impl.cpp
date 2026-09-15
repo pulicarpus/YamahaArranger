2ea1595f229132c79fb6d4e1df97fbe79f7a1e4b
pulicarpus <pulicarpus@gmail.com>
2026-09-15 17:58:59 +0700

    update app/src/main/cpp/tsf_impl.cpp

---
+++ app/src/main/cpp/tsf_impl.cpp
@@ -0,0 +1,4 @@
+// File ini khusus untuk mengaktifkan implementasi TinySoundFont.
+// HARUS ada di SATU file saja untuk menghindari "multiple definition".
+#define TSF_IMPLEMENTATION
+#include "tsf.h"
