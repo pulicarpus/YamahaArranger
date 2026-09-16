4808daa97ceba46985a741d6248a0fb5db7c4a66
pulicarpus <pulicarpus@gmail.com>
2026-09-16 19:34:42 +0700

    Add JNI access to section CASM policies

---
+++ app/src/main/java/com/yourapp/yamahaarranger/style/NativeStyleBridge.kt
@@ -0,0 +1,21 @@
+package com.yourapp.yamahaarranger.style
+
+import javax.inject.Inject
+
+class NativeStyleBridge @Inject constructor() {
+    companion object {
+        init { System.loadLibrary("yamaha_arranger_native") }
+    }
+
+    external fun nativeParseStyle(styBytes: ByteArray): Boolean
+    external fun nativeGetPpq(): Int
+    external fun nativeGetSectionNames(): Array<String>
+    external fun nativeGetSectionLengthTicks(sectionName: String): Int
+    external fun nativeGetPartCount(sectionName: String): Int
+    external fun nativeGetPartName(sectionName: String, partIndex: Int): String
+    external fun nativeGetPartEvents(sectionName: String, partIndex: Int): IntArray
+    external fun nativeGetPartCasm(sectionName: String, partIndex: Int): String
+    external fun nativeFindCasm(styBytes: ByteArray): String
+    external fun nativeExtractVoiceMap(styBytes: ByteArray): String
+    external fun nativeDumpMarkers(styBytes: ByteArray): String
+}
