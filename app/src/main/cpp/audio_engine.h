95f596096acb0e7119762cd8a8a658cd81d26cac
pulicarpus <pulicarpus@gmail.com>
2026-09-22 22:31:22 +0700

    feat: route AudioEngine through BASSMIDI

--- app/src/main/cpp/audio_engine.h
+++ app/src/main/cpp/audio_engine.h
@@ -1,55 +1,37 @@
 #pragma once
-
 #include <oboe/Oboe.h>
 #include <array>
-#include <atomic>
 #include <mutex>
 #include <string>
-#include <vector>
 #include "voice.h"
-#include "soundfont_player.h"
+#include "bassmidi_player.h"
 
 class AudioEngine : public oboe::AudioStreamDataCallback {
 public:
-    static constexpr int kMaxPolyphony = 64;
-
-    bool start();
-    void stop();
-
+    static constexpr int kMaxPolyphony=64;
+    bool start(); void stop();
     bool loadSoundFont(const std::string& path);
     bool loadMelodySoundFont(const std::string& path);
     bool loadDrumSoundFont(const std::string& path);
-    bool isSoundFontLoaded() const { return soundFont_.isLoaded(); }
-    bool isMelodySoundFontLoaded() const { return soundFont_.isMelodyLoaded(); }
-    bool isDrumSoundFontLoaded() const { return soundFont_.isDrumLoaded(); }
+    bool isSoundFontLoaded() const{return soundFont_.isLoaded();}
+    bool isMelodySoundFontLoaded() const{return soundFont_.isMelodyLoaded();}
+    bool isDrumSoundFontLoaded() const{return soundFont_.isDrumLoaded();}
     void unloadSoundFont();
-
-    void noteOn(int midiNote, int rootNote, float velocity01,
-                const float* sampleData, size_t sampleFrames, int sampleRateHz);
-    void noteOff(int midiNote);
-    void allNotesOff();
-
-    void sfNoteOnChannel(int channel, int midiNote, float velocity01);
-    void sfNoteOffChannel(int channel, int midiNote);
-    void sfSetChannelPreset(int channel, int bank, int program);
-    void sfSetChannelMixer(int channel, int volume, int pan, int expression, int reverbSend, int chorusSend);
-    void sfSetChannelExpression(int channel, int expression);
+    void noteOn(int midiNote,int rootNote,float velocity01,const float* sampleData,size_t sampleFrames,int sampleRateHz);
+    void noteOff(int midiNote); void allNotesOff();
+    void sfNoteOnChannel(int channel,int midiNote,float velocity01);
+    void sfNoteOffChannel(int channel,int midiNote);
+    void sfSetChannelPreset(int channel,int bank,int program);
+    void sfSetChannelMixer(int channel,int volume,int pan,int expression,int reverbSend,int chorusSend);
+    void sfSetChannelExpression(int channel,int expression);
     void sfSetMasterGain(float gain);
     std::string sfPresetList() const;
-
-    oboe::DataCallbackResult onAudioReady(oboe::AudioStream* stream,
-                                           void* audioData, int32_t numFrames) override;
-
+    oboe::DataCallbackResult onAudioReady(oboe::AudioStream*,void*,int32_t) override;
 private:
     std::shared_ptr<oboe::AudioStream> stream_;
-    std::array<Voice, kMaxPolyphony> voices_;
+    std::array<Voice,kMaxPolyphony> voices_;
     std::mutex voiceMutex_;
-    int outputSampleRate_ = 48000;
-
-    SoundFontPlayer soundFont_;
-
-    float dcLastInL_ = 0.0f;
-    float dcLastOutL_ = 0.0f;
-    float dcLastInR_ = 0.0f;
-    float dcLastOutR_ = 0.0f;
+    int outputSampleRate_=48000;
+    BassMidiPlayer soundFont_;
+    float dcLastInL_=0,dcLastOutL_=0,dcLastInR_=0,dcLastOutR_=0;
 };
